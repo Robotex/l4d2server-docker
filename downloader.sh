3b64162b64ca11b31e7377dbee797124491cecc2
Robotex <robotex@altervista.org>
2016-08-10 14:34:12 +0800

    Update Dockerfile

--- downloader.sh
+++ downloader.sh
@@ -4,5 +4,5 @@
 
 while [ "${exitcode}" != "0" ]; do
     ./steamcmd.sh +login anonymous +force_install_dir ./serverfiles +app_update 222860 validate +quit
-    local exitcode=$?
+    exitcode=$?
 done
