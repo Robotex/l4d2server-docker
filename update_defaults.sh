67ee8e1c755571808b1a0c3d14fa0d3bb79b3db7
Robotex <robotex@altervista.org>
2016-08-12 01:41:15 +0800

    Update Dockerfile

--- update_defaults.sh
+++ update_defaults.sh
@@ -2,13 +2,14 @@
 # Author: Robotex
 
 find_and_replace() {
-    sed -i "s/$1/$2/g" l4d2server
+    sed -i.bak "s/$1/$2/g" l4d2server
     local retval=$?
     if [ "$retval" != 0 ]
     then
         echo "Default $1 was not found!"
         exit 1
     fi
+    rm l4d2server.bak
 }
 
 find_and_replace 'githubuser="dgibbs64"' 'githubuser="Robotex"'
