4cb05a97ce1517405bc4a8b47d416bbb8bfa54b9
Robotex <robotex@altervista.org>
2017-03-02 00:52:33 +0100

    Update Dockerfile

--- Left 4 Dead 2/entrypoint.sh
+++ Left 4 Dead 2/entrypoint.sh
@@ -2,4 +2,7 @@
 
 /usr/games/steamcmd +runscript /srv/l4d2server/update.txt
 
+mkdir -pv ~/.steam/sdk32/
+ln -s ~/.steam/steamcmd/linux32/steamclient.so ~/.steam/sdk32/steamclient.so
+
 /srv/l4d2server/serverfiles/srcds_run -ip $SRCDS_BIND_IP -port $SRCDS_BIND_PORT -secure +map c5m1_waterfront
