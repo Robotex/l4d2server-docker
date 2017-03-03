50b5bf6493196e0a60e7ac2da0f6f2aa0b83171b
Robotex <robotex@altervista.org>
2017-03-03 18:41:05 +0100

    Update Dockerfile

--- Left 4 Dead 2/start.sh
+++ Left 4 Dead 2/start.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-if [ -f /srv/$GAME/serverfiles/srcds_run ]
+if [ ! -f /srv/$GAME/serverfiles/srcds_run ]
 then
-    /srv/$GAME/serverfiles/srcds_run -ip $BIND_IP -port $BIND_PORT -strictportbind "$@"
-else
     echo "Executable not found!"
+    exit;
 fi
+
+if [ ! -d /srv/${GAME}/addons ]
+then
+    mkdir /srv/${GAME}/addons
+    touch /srv/${GAME}/addons/putyouraddonshere.txt
+    mount --bind --verbose /srv/${GAME}/addons /srv/${GAME}/serverfiles/left4dead2/addons
+fi
+if [ ! -d /srv/${GAME}/cfg ]
+then
+    mkdir /srv/${GAME}/cfg
+    mount --bind --verbose /srv/${GAME}/cfg /srv/${GAME}/serverfiles/left4dead2/cfg
+fi
+
+/srv/$GAME/serverfiles/srcds_run -ip $BIND_IP -port $BIND_PORT -strictportbind "$@"
