05bba2c0b2d09c63ca2db1dc723f3496de865db9
Robotex <robotex@altervista.org>
2017-03-03 17:20:12 +0100

    Update Dockerfile

--- SteamCMD/entrypoint.sh
+++ SteamCMD/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-/usr/games/steamcmd +runscript /srv/$GAME/update.txt
+if [ ! -d /srv/$GAME/serverfiles ]
+then
+    /usr/games/steamcmd +runscript /srv/$GAME/update.txt validate
+else
+    /usr/games/steamcmd +runscript /srv/$GAME/update.txt
+fi
 
 /srv/$GAME/start.sh "$@"
