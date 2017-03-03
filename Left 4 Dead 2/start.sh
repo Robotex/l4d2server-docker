0d2de13c9876bb9649ad52d9f855cc94863db66b
Robotex <robotex@altervista.org>
2017-03-03 15:28:01 +0100

    Update Dockerfile

--- Left 4 Dead 2/start.sh
+++ Left 4 Dead 2/start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ ! -d /srv/$GAME/serverfiles ]; then
+    exit;
+fi
+
 if [ ! -f .speranza ]; then
     # Install MM
     wget http://mirror.pointysoftware.net/alliedmodders/mmsource-1.10.6-linux.tar.gz
@@ -7,8 +11,10 @@ if [ ! -f .speranza ]; then
     rm mmsource-1.10.6-linux.tar.gz
     # Install SM
     wget https://sm.alliedmods.net/smdrop/1.8/sourcemod-1.8.0-git5973-linux.tar.gz
-    tar -zxvf sourcemod-1.8.0-git5973-linux.tar.gz -C /srv/$GAME/sm
+    tar -zxvf sourcemod-1.8.0-git5973-linux.tar.gz -C /srv/$GAME/serverfiles/left4dead2
     rm sourcemod-1.8.0-git5973-linux.tar.gz
+    # Move out SM
+    mv /srv/$GAME/serverfiles/left4dead2/addons/sourcemod /srv/$GAME
     # Lock install
     touch .speranza
 fi
