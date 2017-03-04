f84b62cfe4ecb37805425a8cafeee0c4c24429c5
Robotex <robotex@altervista.org>
2017-03-04 02:41:46 +0100

    Update Dockerfile

--- Left 4 Dead 2/start.sh
+++ Left 4 Dead 2/start.sh
@@ -6,10 +6,18 @@ then
     exit;
 fi
 
-rm -rf /srv/${GAME}/serverfiles/left4dead2/addons
-ln -s /srv/${GAME}/addons /srv/${GAME}/serverfiles/left4dead2
+if [ ! -L /srv/${GAME}/serverfiles/left4dead2/addons ]
+then
+    cp /srv/${GAME}/serverfiles/left4dead2/addons/. /srv/${GAME}/addons
+    rm -rf /srv/${GAME}/serverfiles/left4dead2/addons
+    ln -s /srv/${GAME}/addons /srv/${GAME}/serverfiles/left4dead2
+fi
 
-rm -rf /srv/${GAME}/serverfiles/left4dead2/cfg
-ln -s /srv/${GAME}/cfg /srv/${GAME}/serverfiles/left4dead2
+if [ ! -L /srv/${GAME}/serverfiles/left4dead2/cfg ]
+then
+    cp /srv/${GAME}/serverfiles/left4dead2/cfg/. /srv/${GAME}/cfg
+    rm -rf /srv/${GAME}/serverfiles/left4dead2/cfg /srv/${GAME}/cfg
+    ln -s /srv/${GAME}/cfg /srv/${GAME}/serverfiles/left4dead2
+fi
 
 /srv/$GAME/serverfiles/srcds_run -ip $BIND_IP -port $BIND_PORT -strictportbind "$@"
