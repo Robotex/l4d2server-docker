632f39e348d34213be4269538581c96c047ab25a
Robotex <robotex@altervista.org>
2017-03-04 17:47:01 +0100

    Update Dockerfile

--- Left 4 Dead 2/start.sh
+++ Left 4 Dead 2/start.sh
@@ -6,18 +6,34 @@ then
     exit;
 fi
 
-if [ ! -L /srv/${GAME}/serverfiles/left4dead2/addons ]
+if [ ! -f /srv/${GAME}/addons/.speranza ]
 then
-    cp -r /srv/${GAME}/serverfiles/left4dead2/addons/* /srv/${GAME}/addons
-    rm -rf /srv/${GAME}/serverfiles/left4dead2/addons
-    ln -s /srv/${GAME}/addons /srv/${GAME}/serverfiles/left4dead2
+    if [ ! -L /srv/${GAME}/serverfiles/left4dead2/addons ]
+    then
+        cp -r /srv/${GAME}/serverfiles/left4dead2/addons/* /srv/${GAME}/addons
+        rm -rf /srv/${GAME}/serverfiles/left4dead2/addons
+        ln -s /srv/${GAME}/addons /srv/${GAME}/serverfiles/left4dead2
+    fi
+
+    tar -zxvf -C /srv/${GAME} /tmp/mm.tar.gz
+    tar -zxvf -C /srv/${GAME} /tmp/sm.tar.gz
+    mv -v /srv/${GAME}/addons/sourcemod/* /srv/${GAME}/sourcemod/
+    rm -rf /srv/${GAME}/addons/sourcemod
+    ln -s /srv/${GAME}/sourcemod/ /srv/${GAME}/addons
+
+    touch /srv/${GAME}/addons/.speranza
 fi
 
-if [ ! -L /srv/${GAME}/serverfiles/left4dead2/cfg ]
+if [ ! -f /srv/${GAME}/cfg/.speranza ]
 then
-    cp -r /srv/${GAME}/serverfiles/left4dead2/cfg/* /srv/${GAME}/cfg
-    rm -rf /srv/${GAME}/serverfiles/left4dead2/cfg
-    ln -s /srv/${GAME}/cfg /srv/${GAME}/serverfiles/left4dead2
+    if [ ! -L /srv/${GAME}/serverfiles/left4dead2/cfg ]
+    then
+        cp -r /srv/${GAME}/serverfiles/left4dead2/cfg/* /srv/${GAME}/cfg
+        rm -rf /srv/${GAME}/serverfiles/left4dead2/cfg
+        ln -s /srv/${GAME}/cfg /srv/${GAME}/serverfiles/left4dead2
+    fi
+
+    touch /srv/${GAME}/cfg/.speranza
 fi
 
 /srv/$GAME/serverfiles/srcds_run -ip $BIND_IP -port $BIND_PORT -strictportbind "$@"
