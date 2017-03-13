787effeee8bc2e46e4587eb66f6e25805ac53ed9
Robotex <robotex@altervista.org>
2017-03-13 10:02:59 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,17 @@ then
     exit;
 fi
 
+while test $# -gt 0
+do
+    case "$1" in
+        --install-sm)
+            tar -zxvf /tmp/mm.tar.gz -C /srv/srcds
+            tar -zxvf /tmp/sm.tar.gz -C /srv/srcds
+            ;;
+        --*) echo "bad option $1"
+            ;;
+    esac
+    shift
+done
+
 /srv/srcds/serverfiles/srcds_run -game $SRCDS_GAME -ip $BIND_IP -port 27015 +hostport $BIND_PORT -strictportbind "$@"
