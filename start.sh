26d0047c266387d357800d3e8e9b058f4f537fcf
Robotex <robotex@altervista.org>
2017-03-16 01:47:20 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,15 +6,4 @@ then
     exit;
 fi
 
-while test $# -gt 0
-do
-    case "$1" in
-        --install-sm) tar -zxvf /tmp/mm.tar.gz -C /srv/srcds && tar -zxvf /tmp/sm.tar.gz -C /srv/srcds
-            ;;
-        --*) echo "bad option $1"
-            ;;
-    esac
-    shift
-done
-
-/srv/srcds/serverfiles/srcds_run -autoupdate -steam_dir ~/.steam/steamcmd -steamcmd_script /srv/srcds/update.txt -game $SRCDS_GAME -ip $BIND_IP -port $BIND_PORT -strictportbind +hostname \"${SRCDS_HOSTNAME}\" "$@"
+/srv/srcds/serverfiles/srcds_run "$@" -autoupdate -steam_dir ~/.steam/steamcmd -steamcmd_script /srv/srcds/update.txt -game $SRCDS_GAME -ip $BIND_IP -port $BIND_PORT -strictportbind +hostname \"${SRCDS_HOSTNAME}\"
