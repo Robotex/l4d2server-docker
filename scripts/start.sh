114fa26ac215715d07d41ab4fd5cf424e6324a97
Robotex <robotex@altervista.org>
2017-03-18 21:11:07 +0100

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,4 +6,14 @@ then
     exit;
 fi
 
-/usr/games/gameserver/serverfiles/srcds_run -game $SRCDS_GAME -ip $BIND_IP -port $BIND_PORT -strictportbind "$@" -autoupdate -steam_dir ~/.steam/steamcmd -steamcmd_script /usr/games/gameserver/update.txt +hostname \"${SRCDS_HOSTNAME}\"
+_trap() {
+    echo "---TRAP!---"
+    kill -TERM "$child" 2>/dev/null
+}
+
+trap _trap SIGINT SIGTERM SIGHUP
+
+/usr/games/gameserver/serverfiles/srcds_run -game $SRCDS_GAME -ip $BIND_IP -port $BIND_PORT -strictportbind "$@" -autoupdate -steam_dir ~/.steam/steamcmd -steamcmd_script /usr/games/gameserver/update.txt +hostname \"${SRCDS_HOSTNAME}\" &
+
+child=$! 
+wait "$child"
